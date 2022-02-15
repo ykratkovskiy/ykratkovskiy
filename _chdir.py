9cf203a767570f1bd1a492c39719cd3f888e6e7c
Yuriy Kratkovskiy <ykratkovskiy@gmail.com>
2022-02-15 14:12:28 +0300

    chdir added

--- _chdir.py
+++ _chdir.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+import shutil
+import _mkdir
 
 path = '.\directory'
 list_of_files = sorted(os.listdir(path))
-print (list_of_files)
 
 year =[]
 day = []
@@ -15,30 +16,6 @@ for single_names in list_of_files:
     day.append(unp[1])
     filename.append(unp[2])
 
-print (filename)     
-
-    # shutil.rmtree('.\directory')
-
-for num, original in enumerate(os.listdir('.\directory')):
-    print (num,original)
-    os.rename(original, filename[num])
-
-
-# for num, filename in enumerate ((filename),1):
-#     print (num,filename)
-    
-    
-    # print (original)
-           
-
-# Path('directory2').mkdir(exist_ok=True)
-# for original,fold1, fold2, fl in zip(list_of_files,year,day,filename):
-#     os.rename(f'{original}', f'{filename}')
-    
-    
-    # Path(f'directory2/{fold1}').mkdir(exist_ok=True)
-    # Path(f'directory2/{fold1}/{fold2}').mkdir(exist_ok=True)
-    
-    # Path(f'./directory/{orig}').move(f'/directory2/{fold1}/{fold2}/{fl}')
-    
-    # Path(f'directory2/{fold1}/{fold2}/{fl}').mkdir(exist_ok=True)
+shutil.rmtree('.\directory')
+for fold1,fold2,fl in zip (year,day,filename):
+    Path(f'directory/{fold1}/{fold2}/{fl}').mkdir(parents=True,exist_ok=True)    
