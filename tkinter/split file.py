4feec1b44ff78a66fab20dd4c9fe4a2ff7340650
Yuriy Kratkovskiy <ykratkovskiy@gmail.com>
2022-06-21 10:10:58 +0300

    tkinter

--- tkinter/split file.py
+++ tkinter/split file.py
@@ -2,46 +2,47 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 from tkinter import  StringVar, filedialog
+from turtle import bgcolor
         
        
 window = tk.Tk()
 window.resizable(width=True, height=True)
-window.geometry('+350+150')
-window.title('Разбивка файла')
+window.geometry('+350+100')
+window.title('Split file')
 
 
 
-source_label_msg = tk.Label(window, text='Выберите исходный файл для разбивки')
+source_label_msg = tk.Label(window, text='Выберите исходный файл для разбивки',foreground='maroon',font=18)
 source_label_msg.grid(row=0, columnspan=3)
 
 def source_button():
     filename = filedialog.askopenfilename()
     source_path.set(filename)
-source_buttn = tk.Button(text="Выбрать", command=source_button,width=16,height=1)
+source_buttn = tk.Button(text="Выбрать", command=source_button,width=16,height=1,foreground='maroon')
 source_buttn.grid(row=1, columnspan=3)
 
 source_path = StringVar()
-source_label = tk.Label(textvariable=source_path)
+source_label = tk.Label(textvariable=source_path,foreground='maroon')
 source_label.grid(row=2, columnspan=3)
 
 
 
-target_label_msg = tk.Label(window, text='Выберите директорию для конечных файлов')
+target_label_msg = tk.Label(window, text='Выберите директорию для готовых файлов',foreground='royalblue1',font=18)
 target_label_msg.grid(row=3, columnspan=3)
 
 def target_button():
     pathdirectory = filedialog.askdirectory()
     target_path.set(pathdirectory)
-target_buttn = tk.Button(text="Выбрать", command=target_button,width=16,height=1)
+target_buttn = tk.Button(text="Выбрать", command=target_button,width=16,height=1,foreground='royalblue1')
 target_buttn.grid(row=4, columnspan=3)
 
 target_path = StringVar()
-target_label = tk.Label(textvariable=target_path)
+target_label = tk.Label(textvariable=target_path,foreground='royalblue1')
 target_label.grid(row=5, columnspan=3)
 
 
-label = tk.Label(
-window, text='Введите количество ролей в окне ниже и нажмите кнопку "OK"',).grid(row=6, columnspan=3)
+label = tk.Label(window, text='Введите количество ролей и нажмите кнопку "OK"',foreground='darkgreen',font=18)
+label.grid(row=6, columnspan=3,)
 
 rols_num = tk.Entry(window)
 rols_num.grid(row=7, columnspan=3)
@@ -72,7 +73,7 @@ def ok_button():
             entry_mult.grid(row=i+10, column=1)
             helplist.append(entry_mult)
             button_apply_values = tk.Button(
-            window, text='Записать значения всех кодов', command=write_and_run).grid(row=(int(rols_num.get())+10), columnspan=3)  
+            window, text='Запустить программу', command=write_and_run).grid(row=(int(rols_num.get())+10), columnspan=3)  
             
             if int(rols_num.get())>=100 or int(rols_num.get())<=0:
                 raise messagebox.showerror(title='Error!', message=f"Количество ролей должно быть в диапазоне от 1 до 99!",
@@ -83,7 +84,7 @@ def ok_button():
         onclick=window.quit())
    
 button_ok = tk.Button(
-window, text='OK',command=ok_button,width=8,height=1).grid(row=9, columnspan=3)
+window, text='OK',command=ok_button,width=8,height=1,foreground='darkgreen').grid(row=9, columnspan=3)
 
 
 
