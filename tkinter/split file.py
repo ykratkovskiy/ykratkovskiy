2e6523b219068d1006aedbfd428545a1ed66b674
Yuriy Kratkovskiy <ykratkovskiy@gmail.com>
2022-06-22 20:16:29 +0300

    split file updated

--- tkinter/split file.py
+++ tkinter/split file.py
@@ -1,19 +1,15 @@
+from operator import index
 import tkinter as tk
 from tkinter import messagebox
-import os
 from tkinter import  StringVar, filedialog
-from turtle import bgcolor
 from os import path
 import time
-from webbrowser import open_new
-
-from click import open_file
 
         
        
 window = tk.Tk()
 window.resizable(width=True, height=True)
-window.geometry('+350+100')
+window.geometry('+550+80')
 window.title('Split file')
 
 
@@ -24,7 +20,7 @@ source_label_msg.grid(row=0, columnspan=3)
 def source_button():
     filename = filedialog.askopenfilename()
     source_path.set(filename)
-source_buttn = tk.Button(text="Выбрать", command=source_button,width=16,height=1,foreground='maroon')
+source_buttn = tk.Button(text='Выбрать', command=source_button,width=16,height=1,foreground='maroon')
 source_buttn.grid(row=1, columnspan=3)
 
 source_path = StringVar()
@@ -39,7 +35,7 @@ target_label_msg.grid(row=3, columnspan=3)
 def target_button():
     pathdirectory = filedialog.askdirectory()
     target_path.set(pathdirectory)
-target_buttn = tk.Button(text="Выбрать", command=target_button,width=16,height=1,foreground='royalblue1')
+target_buttn = tk.Button(text='Выбрать', command=target_button,width=16,height=1,foreground='royalblue1')
 target_buttn.grid(row=4, columnspan=3)
 
 target_path = StringVar()
@@ -47,16 +43,23 @@ target_label = tk.Label(textvariable=target_path,foreground='royalblue1')
 target_label.grid(row=5, columnspan=3)
 
 
-label = tk.Label(window, text='Введите количество ролей и нажмите кнопку "OK"',foreground='darkgreen',font=18)
+label = tk.Label(window, text='Введите номера первого и последнено роля в диапазоне',foreground='darkgreen',font=18)
 label.grid(row=6, columnspan=3,)
 
-rols_num = tk.Entry(window)
-rols_num.grid(row=7, columnspan=3)
+label_min = tk.Label(window, text='Первый:',foreground='darkgreen',font=18)
+label_min.grid(row=7, column=0,)
+rol_min = tk.Entry(window, width=8)
+rol_min.grid(row=7, column=1)
+
+label_max = tk.Label(window, text='Последний:',foreground='darkgreen',font=18)
+label_max.grid(row=7, column=2,)
+rol_max = tk.Entry(window, width=8)
+rol_max.grid(row=7, column=3)
 
 
 my_entries = []
 helplist =[]
-
+list_of_numbers = []
 
 
 def write_and_run():
@@ -69,11 +72,11 @@ def write_and_run():
             try:
                 my_entries.append(int(entries.get()))
             except ValueError:
-                messagebox.showerror(title='Error!', message=f"Значением кодов в роле должно быть целое положительное число!",
+                messagebox.showerror(title='Error!', message=f'Значением кодов в роле должно быть целое положительное число!',
                 onclick=window.quit())
             for value in my_entries:
                 if value < 0:
-                    raise messagebox.showerror(title='Error!', message=f"Значением кодов в роле должно быть целое положительное число!",
+                    raise messagebox.showerror(title='Error!', message=f'Значением кодов в роле должно быть целое положительное число!',
                     onclick=window.quit())
     
         src = source_path.get()
@@ -81,48 +84,54 @@ def write_and_run():
     
         full_name = path.basename(f'{src}')
         name = path.splitext(full_name)[0]
-    
         dir_path = path.dirname(src)
-    
+
+        list_of_quantity_of_entries = []
+        count = 0
+        for entry in my_entries:
+            list_of_quantity_of_entries.append(count)
+            count+=1
+            
     
         original_file = open(f'{dir_path}/{full_name}','r')
-        for i in range(len(my_entries)):
-            new_file = open(f'{trg}/{name} rol {i+1}.txt', 'w')
-            c = my_entries[i]
-            for j in range (1,c+1):
-                new_file.write(original_file.readline())
+        for entries_count,number_of_rols in zip(list_of_quantity_of_entries,list_of_numbers):
+                new_file = open(f'{trg}/{name} rol {number_of_rols}.txt', 'w')
+                c = my_entries[entries_count]
+                for j in range (1,c+1):
+                    new_file.write(original_file.readline())
         original_file.close()
         new_file.close()
         messagebox.showinfo(title='Файл разбит', message=f'Успешно выполнено за {round (time.time()-start_time,2)} сек',onclick=window.quit())
     
-    except Exception:
-        messagebox.showerror(title='Ошибка!', message=f"Что-то пошло не так...")
-
-
-
+    except PermissionError:
+        messagebox.showerror(title='Ошибка!', message=f'Не выбран исходный файл или папка для готовых файлов')
 
 
 def ok_button():
     try:
-        for i in range(int(rols_num.get())):
+        rol_minint = int(rol_min.get())
+        rol_maxint = int(rol_max.get())
+        for number_of_rol in range (rol_minint,rol_maxint+1):
+            list_of_numbers.append(number_of_rol)
+        dif = rol_maxint-rol_minint
+        if rol_maxint<=rol_minint:
+            messagebox.showerror(title='Error!', message=f'Значение "первого" роля должно быть меньше значения "второго" роля!',
+            onclick=window.quit())
+        for i in range (rol_minint-1,rol_maxint):
             label_rols = tk.Label(window, text=f'Введите количество кодов в роле № {i+1}').grid(row=i+10, column=0)
             entry_mult = tk.Entry(window)
             entry_mult.grid(row=i+10, column=1)
             helplist.append(entry_mult)
             button_apply_values = tk.Button(
-            window, text='Запустить программу', command=write_and_run).grid(row=(int(rols_num.get())+10), columnspan=3)  
+            window, text='Запустить программу', command=write_and_run).grid(row=(dif+1000), columnspan=3)  
             
-            if int(rols_num.get())>=100 or int(rols_num.get())<=0:
-                raise messagebox.showerror(title='Error!', message=f"Количество ролей должно быть в диапазоне от 1 до 99!",
-                onclick=window.quit())
-    
     except ValueError:
-        messagebox.showerror(title='Error!', message=f"Значением количества ролей должно быть целое положительное число!",
+        messagebox.showerror(title='Error!', message=f'Значением количества ролей должно быть целое положительное число!',
         onclick=window.quit())
    
 button_ok = tk.Button(
 window, text='OK',command=ok_button,width=8,height=1,foreground='darkgreen').grid(row=9, columnspan=3)
 
 
-
 window.mainloop()
+print ()
--- tkinter/train.py
+++ tkinter/train.py
@@ -1,13 +1,28 @@
-import re
-import pathlib
+# import re
+# import pathlib
 
-file = '87 PrM kefir 3,2 1L rol_166_vse.csv'
-name = file.split('_vse')[0]
-ext = pathlib.Path(file).suffix
-name_for_rep = name.split('_')[0]
-# print (name)
-# print (ext)
-print (name_for_rep)
+# file = '87 PrM kefir 3,2 1L rol_166_vse.csv'
+# name = file.split('_vse')[0]
+# ext = pathlib.Path(file).suffix
+# name_for_rep = name.split('_')[0]
+# # print (name)
+# # print (ext)
+# print (name_for_rep)
 
-rol_number = re.findall(r'_\d+',name)
-print (rol_number)
+# rol_number = re.findall(r'_\d+',name)
+# print (rol_number)
+
+
+
+my_entries = [45,67,11]
+# newlist = []
+# count = 0
+# for i in my_entries:
+#     newlist.append(count)
+#     count+=1
+# for j in newlist:
+#     print (j)
+
+
+for i in range (len(my_entries)):
+    print (i)
